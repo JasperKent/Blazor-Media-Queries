638b2f65b5ce79f76fe219b054364bad0d02055c
Jasper Kent <software@jasperkent.com>
2024-06-21 08:55:22 +0100

    Add project files.

---
+++ BlazorMediaQueries/Program.cs
@@ -0,0 +1,13 @@
+using BlazorMediaQueries;
+using BlazorPro.BlazorSize;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMediaQueryService();
+
+await builder.Build().RunAsync();
